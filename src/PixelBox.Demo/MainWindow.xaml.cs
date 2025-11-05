0c31c5d88b889baa31f117e9d72854e05a54b526
Jad Altahan <xv.git@aol.com>
2025-11-05 13:46:26 -0800

    Adjust refresh interval in demo

--- src/PixelBox.Demo/MainWindow.xaml.cs
+++ src/PixelBox.Demo/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ public partial class MainWindow : Window
 
         _magWindowCfg = new PixelMagnifierWindowConfig 
         {
-            RefreshInterval = 15
+            RefreshInterval = 30
         };
     }
 
